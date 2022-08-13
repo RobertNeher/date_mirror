5157f57fbf741db0f3b15716a62d6aff90cda956
RobertNeher <Robert.Neher@gmail.com>
2022-08-13 15:37:08 +0200

    equibrium renamed to solstice

--- date_mirror.py
+++ date_mirror.py
@@ -2,7 +2,7 @@ import datetime
 
 if __name__ == "__main__":
     today_year = datetime.datetime.today().strftime("%y")
-    EQULIBRIUM = datetime.datetime.strptime("21.06." + today_year, "%d.%m.%y")
-    day_diff = datetime.datetime.today() - EQULIBRIUM
-    mirror_day = EQULIBRIUM - datetime.timedelta(days=day_diff.days)
+    SOLSTICE = datetime.datetime.strptime("21.06." + today_year, "%d.%m.%y")
+    day_diff = datetime.datetime.today() - SOLSTICE
+    mirror_day = SOLSTICE - datetime.timedelta(days=day_diff.days)
     print("Todays date is equal to %s" % datetime.datetime.strftime(mirror_day, "%d. %B %Y"))
